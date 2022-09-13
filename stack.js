e218e17bd5c4aac8ae11991ab9fa2392d0359bf6
Brian Elizondo <brian_elizondo@msn.com>
2022-09-13 17:37:16 -0400

    Data Structures => Stacks & Queues Exercises: Stacks, Make a Stack class. It should include methods for pushing, popping, peeking, and checking if the stack is empty. Make it throw an error if you try to pop from an empty stack

--- stack.js
+++ stack.js
@@ -1,46 +1,60 @@
 /** Node: node for a stack. */
-
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
-
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+    /** push(val): add new value to start of the stack. Returns undefined. */
+    push(val) {
+        let newNode = new Node(val);
+        if(this.first === null){
+            if (this.first === null) this.first = newNode;
+            if (this.last !== null) this.last.next = newNode;
+            this.last = newNode;
+        }else{
+            newNode.next = this.first;
+            this.first = newNode;
+        }
+        this.size++;
+    }
 
-  push(val) {
+    /** pop(): remove the node from the top of the stack
+    * and return its value. Should throw an error if the stack is empty. */
+    pop() {
+        if(this.first === null){
+            throw new Error("The queue is empty");
+        }else{
+            let currentFirst = this.first;
+            this.first = this.first.next;
+            this.size--;
+            if(this.size == 0){
+                this.first = null;
+                this.last = null;
+            }
+            return currentFirst.val;
+        }
+    }
 
-  }
+    /** peek(): return the value of the first node in the stack. */
+    peek() {
+        return this.first.val;
+    }
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
-
-  pop() {
-
-  }
-
-  /** peek(): return the value of the first node in the stack. */
-
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+    /** isEmpty(): return true if the stack is empty, otherwise false */
+    isEmpty() {
+        return this.size === 0 ? true : false;
+    }
 }
 
 module.exports = Stack;
